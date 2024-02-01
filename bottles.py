26e1de39c7822cba44e7ac5d368ee2f5ff4dc8c5
dlibberton <drew.libberton@gmail.com>
2024-02-01 16:44:38 -0500

    initial solution

--- bottles.py
+++ bottles.py
@@ -1,7 +1,20 @@
-def bottle_song():
-	# write your code here! changes
-	pass
-
+"""def bottle_song():
+	num = 99
+	bottle = "bottles"
+	while num > -1:
+		song = (f"{num} {bottle} of beer on the wall, {num} {bottle} of beer.Take one down and pass it around, {num - 1} {bottle} of beer on the wall.")
+		if num > 2:
+			print(song)
+			num = num - 1
+		elif num == 0:
+			print("No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.")
+			num = num - 1
+		elif num == 2: 
+			bottle = "bottle"
+			print("2 bottles of beer on the wall, 2 bottles of beer. Take one down and pass it around, 1 more bottle of beer on the wall.")
+			num = num - 1
+		elif num == 1:
+			print("1 bottle of beer on the wall, 1 bottle of beer.Take one down and pass it around, no more bottles of beer on the wall.")
+			num = num - 1
 bottle_song()
-
-####kldjsfalkfdajalskdfjdfls
+"""
--- foo.py
+++ foo.py
@@ -1,1 +1,4 @@
-print('I am so hungry right now.')
+from bottles import bottle_song
+
+def test_bottles(): 
+    assert test_bottles(3) == f"3 bottles of beer on the wall, 3 bottles of beer. Take one down and pass it around, 3 bottles of beer on the wall."
